ad60f6a42e199b5e1f75d3971e47dcd4777471a2
Iris-Kuzmenko <kuzmenkoiris@gmail.com>
2020-07-13 07:11:40 -0500

    adds user show route

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -14,18 +14,8 @@ class Api::UsersController < ApplicationController
     end
   end
 
-#   # def show
-#   #   @user = User.find_by(params[:id])
-#   #   if @user.save
-#   #     render "show.json.jb"
-#   #   else
-#   #     render json: { errors: user.errors.full_messages }, status: :bad_request
-#   #   end
-#   # end
-
-#   def update
-#   end
-
-#   def destroy
-#   end
-# end
+  def show
+    @user = User.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+end
