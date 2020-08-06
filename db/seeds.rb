59901151ec1c721141b67683e07722d3981a58e0
Iris-Kuzmenko <kuzmenkoiris@gmail.com>
2020-08-06 12:16:54 -0500

    adds items to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,49 +1,114 @@
-# DrySetting.create!([
-#   { setting: "normal tumble dry low" },
-#   { setting: "normal tumble dry medium" },
-#   { setting: "normal tumble dry high" },
-#   { setting: "delicate tumble dry low" },
-#   { setting: "delicate tumble dry medium" },
-#   { setting: "delicate tumble dry high" },
-#   { setting: "permanent press tumble dry low" },
-#   { setting: "permanent press tumble dry medium" },
-#   { setting: "permanent press tumble dry high" },
-#   { setting: "no heat tumble dry" },
-#   { setting: "drip dry" },
-#   { setting: "line dry" },
-#   { setting: "hang dry" },
-#   { setting: "dry in shade" },
-# ])
-# WashSetting.create!([
-#   { setting: "tumble dry low, normal" },
-#   { setting: "normal cold wash" },
-#   { setting: "normal warm wash" },
-#   { setting: "normal hot wash" },
-#   { setting: "delicate cold wash" },
-#   { setting: "delicate warm wash" },
-#   { setting: "delicate hot wash" },
-#   { setting: "permanent press cold wash" },
-#   { setting: "permanent press warm wash" },
-#   { setting: "permanent press hot wash" },
-#   { setting: "hand wash only" },
-# ])
-
-# Category.create!([
-#   { name: "tops" },
-#   { name: "bottoms" },
-#   { name: "dresses" },
-#   { name: "sleepwear" },
-#   { name: "undergarments" },
-#   { name: "outerwear" },
-#   { name: "other" },
-# ])
-
-# User.create!([
-#   { username: "Chana", email: "chana@gmail.com", password: "password", password_confirmation: "password", admin: "false" },
-# ])
-
 Item.create!([
-  { name: "red T-shirt", wash_setting_id: 1, dry_setting_id: 1, category_id: 1, user_id: 2 },
-  { name: "blue long-sleeved tee", wash_setting_id: 2, dry_setting_id: 1, category_id: 1, user_id: 2 },
-  { name: "underwear", wash_setting_id: 4, dry_setting_id: 10, category_id: 5, user_id: 2 },
+  {
+    name: "red T-shirt", wash_setting_id: 2, dry_setting_id: 1, category_id: 1, user_id: 2,
+  },
+  {
+    name: "blue long-sleeved tee", wash_setting_id: 2, dry_setting_id: 2, category_id: 1, user_id: 2,
+  },
+  {
+    name: "light wash skinny jeans", wash_setting_id: 3, dry_setting_id: 1, category_id: 2, user_id: 2,
+  },
+  {
+    name: "dark wash skinny jeans", wash_setting_id: 2, dry_setting_id: 1,
+    category_id: 2, user_id: 2,
+  },
+  {
+    name: "Athleta black leggings", wash_setting_id: 8, dry_setting_id: 7,
+    category_id: 2, user_id: 2,
+  },
+  {
+    name: "black turtleneck dress",
+    wash_setting_id: 2, dry_setting_id: 1, category_id: 3,
+    user_id: 2,
+  },
+  {
+    name: "baseball cap",
+    category_id: 7,
+    wash_setting_id: 11, dry_setting_id: 73,
+    user_id: 2,
+  },
+  {
+    name: "socks",
+    category_id: 7,
+    wash_setting_id: 2, dry_setting_id: 1,
+    user_id: 2,
+  },
+  {
+    name: "striped pajama pants",
+    category_id: 2,
+    wash_setting_id: 8, dry_setting_id: 10,
+    user_id: 2,
+  },
+  {
+    name: "underwear",
+    category_id: 5,
+    wash_setting_id: 2, dry_setting_id: 1,
+    user_id: 2,
+  },
+  {
+    name: "brown cashmere sweater",
+    category_id: 1,
+    wash_setting_id: 11, dry_setting_id: 73,
+    user_id: 2,
+  },
+  {
+    name: "Lululemon black leggings",
+    category_id: 2,
+    wash_setting_id: 9, dry_setting_id: 7,
+    user_id: 2,
+  },
+  {
+    name: "white tank top",
+    category_id: 1,
+    wash_setting_id: 2, dry_setting_id: 1,
+    user_id: 2,
+  },
+  {
+    name: "Citizens of Humanity flare jeans",
+    category_id: 2,
+    wash_setting_id: 8, dry_setting_id: 14,
+    user_id: 2,
+  },
+  {
+    name: "black dress pants",
+    category_id: 2,
+    wash_setting_id: 8,
+    dry_setting_id: 14,
+    user_id: 2,
+  },
+  {
+    name: "white silk blouse",
+    category_id: 1,
+    wash_setting_id: 11,
+    dry_setting_id: 73,
+    user_id: 2,
+  },
+  {
+    name: "tropical t-shirt",
+    category_id: 1,
+    wash_setting_id: 3,
+    dry_setting_id: 1,
+    user_id: 2,
+  },
+  {
+    name: "grey hoodie",
+    category_id: 1,
+    wash_setting_id: 2,
+    dry_setting_id: 1,
+    user_id: 2,
+  },
+  {
+    name: "pink lace top",
+    category_id: 1,
+    wash_setting_id: 11,
+    dry_setting_id: 73,
+    user_id: 2,
+  },
+  {
+    name: "black puffy coat",
+    category_id: 6,
+    wash_setting_id: 2,
+    dry_setting_id: 1,
+    user_id: 2,
+  },
 ])
